ab1ef7b1b844086faf1acd273546e1d025dc3038
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-08-16 15:16:24 -0300

    Aula 4 pratica de sintaxe

---
+++ pratica_sintaxe/pratica_sintaxe.py
@@ -0,0 +1,10 @@
+numeroDigitadoPeloUsuario = int(input('Digite um número: '))
+
+if (numeroDigitadoPeloUsuario % 2) == 0 :
+ print(f'{numeroDigitadoPeloUsuario} é um número Par')
+else:
+ print(f'{numeroDigitadoPeloUsuario} é um número Impar')
+
+#Padrão de escrita de código em Python CameLCase;
+#Padrão de escrita de código em Python SnakeCase;
+#O padrão de escrita de código em Python é definido pela PEP 8
