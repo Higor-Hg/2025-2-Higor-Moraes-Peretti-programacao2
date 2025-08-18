c04743556e8b18f45c451583e82291bc899b0192
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-08-18 15:08:53 -0300

    Conceito de variável
    Aula5

---
+++ Conseitos_Gerais/conceito_variavel.py
@@ -0,0 +1,23 @@
+numero1 = 10
+print("Meu 1 numero: ",numero1)
+
+
+'''
+1º - numero1 é uma variável. Baseado na interência, sera deduzido como uma variável do tipo int (ineiro);
+
+2º - opereador de atribuição = (símbolo de igual);
+
+3º - 10, constante do tipo inteiro;
+
+4º - print = função do python p/ imprimir na tela (stdout);
+
+5º - (), exibido na cor amarela, representa a lista de argumentos da função "print". USO OBRIGATÓRIO;
+
+6º - "Meu primeiro número" é o primneiro argumento da função print, que nesse caso é do String;
+
+7º - VIRGULA(,) - dentro da lista de argumentos da função print, a virgula separa os argumentos (ou parâmetros).
+É dessa forma, que se indentifica o número de argumentos de uma função no python;
+
+8º - variável numero1 - representa o segundo argumento (ou parâmetro) da função print;
+
+'''
