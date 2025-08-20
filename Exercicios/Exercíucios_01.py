33a8d245a6d2e6217af08d76f29fa658ef1f775e
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-08-20 17:17:53 -0300

    Exercícios_Resolovidos
    Feito

---
+++ Exercicios/Exercíucios_01.py
@@ -0,0 +1,85 @@
+  #Exercícios: variáveis, tipos de dados, comentários 
+
+#Aluno: Higor Moraes Peretti
+#Data:20/08
+
+#RESPOSTAS
+
+#Exercício 01:
+#Resposta:
+#Aula de relsolução de exercícios.
+
+#Exercício 02:
+#Resposta:
+'''
+Aula
+5
+Resolvendo
+meus
+Exercícios
+'''
+#Exercício 03:
+#Resposta:
+
+print("Boa tarde pessoal da disciplina de Programação II.")
+
+#Exercício 04:
+#Resposta:
+
+nome      = "Higor"     
+idade     = 16            
+altura    = 1.77        
+
+#Exercício 05:
+#Resposta:
+
+valor   = "456"
+
+print("Valor da variável:", valor)
+print("Tipo de dado:", type(valor))
+
+#Exercício 06:
+#Resposta:
+
+numero = input("Digite um número:  ")
+
+print("Você digitou o número: ", numero)
+
+#Exercício 07:
+#Resposta:
+
+numero1   = 346
+numero2   = 10
+soma      = numero1 + numero2
+
+print("O resultado da soma é: ", soma)
+
+#Exercício 08:
+#Resposta:
+
+a   = 15
+b   = 7
+
+resultado = a - b
+
+print("O resultado da subtração é: ", resultado)
+
+#Exercício 09:
+#Resposta:
+
+numero1             = 230
+numero2             = 110
+multiplicacao       = numero1 * numero2
+
+print("O resultado da multiplicação é: ", multiplicacao)
+
+#Exercício 10:
+#Resposta:
+
+numero1             = 30
+numero2             = 10
+divisao       = numero1 / numero2
+
+print("O resultado da divisão é: ", divisao)
+
+
