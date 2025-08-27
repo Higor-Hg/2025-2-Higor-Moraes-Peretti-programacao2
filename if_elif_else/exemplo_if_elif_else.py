3f290cdf28ae1fc1d1a0f19407b0d08d085645b9
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-08-27 15:14:48 -0300

    exemplo-if-elif-else
    exemplo

---
+++ if_elif_else/exemplo_if_elif_else.py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Primeiro Número: "))
+num2 = int(input("Segundo Número: "))
+
+if num1 > num2:
+    print ("O primeiro nu mero é o maior!")
+        
+if num2 > num1:
+    print ( "O segundo número é maior!")
