0764d452f867ef993afe953ef62be52de746e417
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-09-17 17:25:56 -0300

    Funcoes
    conceito_funcoes

--- funcoes/conceito_funcoes.py
+++ funcoes/conceito_funcoes.py
@@ -2,9 +2,25 @@
 def triplo(calcular_triplo):
     return calcular_triplo * 3 #Implementação da função
 
+#Função que possui 2 parâmetros e nenhum retorno
+def imprime_info(nome, idade) :
+    print("Nome: ", nome)
+    print("Idade: ", idade)
+
+''''
 #Código principal
-num = triplo(3) #chamda da funções
+calcular_triplo = 3
+num = triplo(calcular_triplo) #chamda da funções
 print("O triplo de 3 é: ",num) #num nesse caso vai valer 9, pois 3 * 3 = 9
 
-num = triplo(6) #chamada da função
+calcular_triplo = 6
+num = triplo(calcular_triplo) #chamada da função
 print("O triplo de 6 é: ",num) #num nesse caso vai valer 18, pois 6 * 3 = 18
+'''
+
+
+
+#Código principal    
+nome = 'Jhon Travolta'
+i = 52
+imprime_info(nome, i) #Chamada da função
