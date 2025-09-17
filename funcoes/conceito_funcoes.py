36d68fcdd40d247f53703001c3ab5371de4a60dd
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-09-17 16:32:04 -0300

    funcoes
    conceitos_funcoes

--- funcoes/conceito_funcoes.py
+++ funcoes/conceito_funcoes.py
@@ -1,10 +1,10 @@
 #Função que calcula o triplo de um número
-def triplo(calculartriplo):
-    return x * 3 #Implementação da função
+def triplo(calcular_triplo):
+    return calcular_triplo * 3 #Implementação da função
 
 #Código principal
 num = triplo(3) #chamda da funções
-print("O triplo de 3 é: ", num) #num nesse caso vai valer 9, pois 3 * 3 = 9
+print("O triplo de 3 é: ",num) #num nesse caso vai valer 9, pois 3 * 3 = 9
 
 num = triplo(6) #chamada da função
 print("O triplo de 6 é: ",num) #num nesse caso vai valer 18, pois 6 * 3 = 18
