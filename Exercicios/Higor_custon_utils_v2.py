0a91aade0c06eccc64a25e202aece4bb3dab87c1
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-10-01 17:27:38 -0300

    Iniciacao-de-resolucao-de-exercicios
    ate-onde-fui-na-aula

---
+++ Exercicios/Higor_custon_utils_v2.py
@@ -0,0 +1,105 @@
+import re
+
+menu = """
+========= MENU =========
+1 - Operadores
+2 - Palavras Reservadas no Python
+3 - Tipos de dados no Python
+4 - Conceito de Variável
+5 - Verificar Ano Bissexto
+6 - Contar vogais da frase
+7 - Contar palavras da frase
+8 - Inverter a frase
+9 - Validar senha
+10 - Validar formato e tamanho de um CPF
+11 - Verificar triângulo
+Sair - Finalizar programa
+=========================
+"""
+operadores = 'Aritméticos: + (soma), - (subtração), * (multiplicação)\nComparação: == (igual), != (diferente), > (maior)\nLógicos: and, or, not'
+Palavras_Reservadas = 'if, else, for, while, break, continue, def, return, class, import' 
+Tipos_de_dados =
+Conceito_de_Variável =
+Verificar_Ano_bissexto =
+vogais_da_frase =
+palavras_da_fras =e
+Inverter_frase =
+Validar_senha =
+Validar_CPF =
+Verificar_triângulo =
+
+
+#programa principal
+print(menu)
+
+menu = int(input("Digite o número que corresponde a qual você BUSCA no menu: "))
+    
+if menu == 1:
+    print("Operadores")
+    print(operadores)
+
+elif menu == 2:  
+    print("Palavras reservadas:")
+    print(Palavras_Reservadas)
+
+elif menu == 3:
+    print("")
+    print()
+elif menu == 4:
+    print("")
+    print()
+elif menu == 5:
+    print("")
+    print()
+elif menu == 6:
+    print("")
+    print()
+elif menu == 7:
+    print("")
+    print()
+elif menu == 8:
+    print("")
+    print()
+elif menu == 9:
+    print("")
+    print()
+elif menu == 10:
+    print("")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
