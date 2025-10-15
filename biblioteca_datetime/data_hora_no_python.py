495927ec441a433a203c8e52c339332257917005
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-10-15 16:17:47 -0300

    Exercicios-Finalizados
    Finalização-dos-exercicios-7-8-9-e-10-biblioteca-datetime-lista1

--- biblioteca_datetime/data_hora_no_python.py
+++ biblioteca_datetime/data_hora_no_python.py
@@ -43,17 +43,45 @@ def data_formatada():
 
 '''6. Crie um programa que converta uma string no formato "2023-11-26" para um objeto
 "date";'''
+def converter_string(data_string):
+    
+    objeto_datetime = datetime.strptime(data_string, '%Y-%m-%d')
+    
+    objeto_date = objeto_datetime.date()
+    
+    return objeto_date
 
-def convert_frase():
-    conversao = 
+#7. Crie um programa que obtenha o timestamp atual em segundos desde 01/01/1970.
+def timestamp_atual():
+  
+  timestamp = datetime.now().timestamp()
+  return timestamp
+
 
 #8. Crie um programa que adiciona 7 dias à data atual.
+def adicionar_dias_data_atual(dias):
+  data_atual = date.today()
+  nova_data = data_atual + timedelta(days=dias)
+  return nova_data
+
+
 
 #9. Crie um programa que subtraia 30 dias da data atual.
+def subtrair_dias_data_atual(dias):
+  
+    data_atual = date.today()
+    nova_data = data_atual - timedelta(days=dias)
+    return nova_data
 
 '''10. Crie uma função que receba duas datas diferentes como parâmetro. A função deve
 retornar um valor inteiro que represente a diferença entre essas datas passadas por
 parâmetro.'''
+
+def dif_entre_datas(data1, data2):
+
+  diferenca = data2 - data1
+  return diferenca.days
+
 
 
 #PROGRAMA PRINCIPAL
@@ -80,6 +108,33 @@ data_frt = data_formatada()
 print("Data Atual:", data_frt)
 
 #Q6
+data_em_string = "2023-11-26"
+data_em_objeto = converter_string(data_em_string)
 
+print(f"String original: {data_em_string}")
+print(f"Tipo da string: {type(data_em_string)}")
+print("-" * 30)
+print(f"Objeto date: {data_em_objeto}")
+print(f"Tipo do objeto: {type(data_em_objeto)}")
   
+#Q7
+timestamp_segundos = timestamp_atual()
+print(f"O timestamp atual em segundos é: {timestamp_segundos}")
 
+#Q8
+data_futura = adicionar_dias_data_atual(7)
+print(f"Data atual: {date.today()}")
+print(f"Data daqui a 7 dias: {data_futura}")
+
+#Q9
+dias_para_subtrair = 30
+data_resultante = subtrair_dias_data_atual(dias_para_subtrair)
+print(f"Data atual: {date.today()}")
+print(f"Data 30 dias atrás: {data_resultante}")
+
+#Q10
+data_inicial = date(2025, 1, 1)
+data_final = date(2025, 10, 15)
+
+dias_de_diferenca = dif_entre_datas(data_inicial, data_final)
+print(f"A diferença entre as datas é de {dias_de_diferenca} dias.")
