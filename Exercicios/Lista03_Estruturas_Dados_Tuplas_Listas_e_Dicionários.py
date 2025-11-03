94a08bc49e28ce2fa7d8f88bed6689ff12fcaa4a
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-11-03 15:17:52 -0300

    list3
    Lista03_Estruturas_Dados_Tuplas_Listas_e_Dicionários

---
+++ Exercicios/Lista03_Estruturas_Dados_Tuplas_Listas_e_Dicionários.py
@@ -0,0 +1,105 @@
+'''
+1. A partir de uma sequência de números digitados pelo usuário, separados por
+vírgula, converta tal sequência numérica em uma lista. Crie uma função que
+receba como parâmetro a sequência de números, e retorne uma lista. Crie
+uma segunda função que receba uma lista e a imprima na tela.
+'''
+
+def sequencia()
+    print()
+
+
+
+
+
+
+
+
+'''
+2. Crie uma função que receba dois parâmetros: 1 - uma lista de números
+inteiros; 2 - crescente ou decrescente. Se for chamada com o segundo
+parâmetro = crescente, ordene e retorne a lista de números em ordem
+crescente. Se for chamada com o segundo parâmetro = decrescente, ordene
+e retorne a lista de números em ordem decrescente. Utilize a função de
+impressão de estrutura de dados criada na questão 1 e imprima na tela a lista
+retornada. Teste a função na ordem crescente e na decrescente. Faça com
+que o usuário forneça a lista de números.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+3. Crie uma função que receba duas listas de números informadas pelo usuário,
+de tamanhos idênticos, e retorne uma nova lista que contenha apenas os
+elementos comuns às duas listas, sem elementos duplicados. Utilize a função
+de impressão de estrutura de dados criada na questão 1 e imprima na tela a
+lista retornada. Fiquem atentos às responsabilidades do programa principal
+nessa implementação e as responsabilidades da função.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+4. Crie uma função que receba uma lista do tipo String e retorne um dicionário
+onde cada elemento da lista será uma chave do dicionário e o valor vinculado
+a cada chave será o tamanho da chave String. Crie uma segunda função que
+imprima o dicionário retornado pela primeira função. Exemplo de uma
+chave\valor do dicionário retornado pela função: “Python”: 6.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+5. Crie uma função que receba 02 listas, de mesmo tamanho, e retorne uma
+lista de tuplas contendo os elementos das duas listas passadas por
+parâmetro. Pesquise e utilize a função “zip()” nessa implementação. Imprima
+a lista de tuplas, avalie se as funções de impressão já implementadas podem
+ser usadas, caso não possa, crie uma nova função para impressão de listas
+de tuplas.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#PROGRAMA PRINCIPAL
