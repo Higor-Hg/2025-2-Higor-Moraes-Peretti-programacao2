5a7bd6a0c18d766aaa4ec57cfe42c688f07edc99
Higor Moraes Peretti <higormoraesperetti@gmail.com>
2025-12-07 19:17:29 -0300

    Deixando o repositório apresentével
    isso ai q ta no título

---
+++ Exercicios/Exercicios_01_variáveis_tipos_de_dados_comentários.py
@@ -0,0 +1,73 @@
+# ==================================================
+# Exercícios: Variáveis, Tipos de Dados e Comentários
+# Aluno: Higor Moraes Peretti
+# Data: 20/08
+# ==================================================
+
+# Exercício 01
+# Comentário de uma linha
+# Aula de resolução de exercícios.
+
+# Exercício 02
+# Comentário de mais de uma linha
+"""
+Aula
+5
+Resolvendo
+meus
+Exercícios
+"""
+
+# Exercício 03
+# Mostrar mensagem na tela
+print("Boa tarde pessoal da disciplina de Programação II.")
+
+# Exercício 04
+# Criação de variáveis com tipos diferentes
+nome   = "Higor"
+idade  = 16
+altura = 1.77
+
+# Exercício 05
+# Exibir valor e tipo da variável
+valor = "456"
+
+print("Valor da variável:", valor)
+print("Tipo de dado:", type(valor))
+
+# Exercício 06
+# Ler um número digitado pelo usuário
+numero = input("Digite um número: ")
+print("Você digitou o número:", numero)
+
+# Exercício 07
+# Soma de dois números
+numero1 = 346
+numero2 = 10
+soma = numero1 + numero2
+
+print("O resultado da soma é:", soma)
+
+# Exercício 08
+# Subtração de dois números
+a = 15
+b = 7
+resultado = a - b
+
+print("O resultado da subtração é:", resultado)
+
+# Exercício 09
+# Multiplicação de dois números
+numero1 = 230
+numero2 = 110
+multiplicacao = numero1 * numero2
+
+print("O resultado da multiplicação é:", multiplicacao)
+
+# Exercício 10
+# Divisão de dois números
+numero1 = 30
+numero2 = 10
+divisao = numero1 / numero2
+
+print("O resultado da divisão é:", divisao)
